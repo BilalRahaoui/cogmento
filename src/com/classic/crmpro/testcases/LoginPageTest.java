446173fe0d4c25a326a53c14c14ff4cd5ba57d11
Bilal Rahaoui <rahaoui.bilalma@gmail.com>
2019-03-05 20:42:42 +0100

    add new test case for login

--- src/com/classic/crmpro/testcases/LoginPageTest.java
+++ src/com/classic/crmpro/testcases/LoginPageTest.java
@@ -19,18 +19,15 @@ public class LoginPageTest {
 		driver.manage().window().maximize();
 		driver.get("https://fr.cogmento.com");
 	}
-
 	@AfterMethod
 	public void exitAll() {
 		driver.quit();
 	}
-
 	@Test (priority = 1)
 	public void testUrl() {
 		String expectedResult = "https://fr.cogmento.com/";
 		String actuelResult = driver.getCurrentUrl();
 		Assert.assertEquals(actuelResult,expectedResult,"Failed because links not matches");
-		
 	}
 	@Test (priority = 2)
 	public void testTitle() {
@@ -41,14 +38,25 @@ public class LoginPageTest {
 		soft.assertAll();
 
 	}
-
 	@Test (priority = 3)
 	public void testLogo() {
 		WebElement logo = driver.findElement(By.xpath("//div[@class='rd-navbar-panel']"));
 		boolean actuelResult = logo.isDisplayed();
 		Assert.assertTrue(actuelResult);
 	}
+	@Test (priority=4)
+	public void testLogin() {
+		WebElement loginButton = driver.findElement(By.xpath("//span[contains(text(),'Log In')]"));
+		loginButton.click();
+		WebElement email = driver.findElement(By.name("email"));
+		email.sendKeys("rahaoui.bilalma@gmail.com");
+		WebElement password = driver.findElement(By.name("password"));
+		password.sendKeys("Bmn123456");
+		WebElement loginButton2 = driver.findElement(By.xpath("//div[@class='ui fluid large blue submit button']"));
+		loginButton2.click();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
+	}
 
 
 }
